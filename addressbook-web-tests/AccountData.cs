04f1d7670d2f9aebd417ff8f6598619193f4592f
Kuzina-E <ekaterina_kuzina_7@mail.ru>
2020-10-17 15:05:00 +0300

    Созданы вспомогательный класс для хранения свойств группы контактов

---
+++ addressbook-web-tests/AccountData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace WebAddressbookTests
+{
+    public class AccountData
+    {
+        private string username;
+        private string password;
+
+        public AccountData(string username, string passwors)
+        {
+            this.username = username;
+            this.password = passwors;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+    }
+}
---
+++ addressbook-web-tests/GroupData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace WebAddressbookTests
+{
+    public class GroupData
+    {
+        private string name;
+        private string heder= "";
+        private string footer = "";
+
+        public GroupData(string name)
+        {
+            this.name = name;
+        }
+
+       // public GroupData(string name, string heder, string footer)
+       // {
+       //     this.name = name;
+      //      this.heder = heder;
+       //     this.footer = footer;
+      //  }
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Heder
+        {
+            get
+            {
+                return heder;
+            }
+            set
+            {
+                heder = value;
+            }
+        }
+
+        public string Footer
+        {
+            get
+            {
+                return footer;
+            }
+            set
+            {
+                footer = value;
+            }
+        }
+    }
+}
