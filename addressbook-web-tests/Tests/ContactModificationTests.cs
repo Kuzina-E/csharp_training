e112e9f39b7c69044d7d27303cf45f6662c8a349
Kuzina-E <ekaterina_kuzina_7@mail.ru>
2020-10-19 20:52:54 +0300

    Созданы тесты создания, удаления, модификации для контактов

---
+++ addressbook-web-tests/Tests/ContactModificationTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactModificationTests : TestBase
+    {
+
+
+        [Test]
+        public void ContactModificationTest()
+        {
+            ContactData newData = new ContactData("Petr", "Petrov");
+            app.Contacts.Modify(1, newData);
+        }
+    }
+}
---
+++ addressbook-web-tests/Tests/ContactRemovalTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactRemovalTests: TestBase
+    {
+        [Test]
+        public void RemoveContactTest()
+        {
+            app.Contacts.Remove(1);
+
+        }
+    }
+}
