c4b8a7c7d9d7e3595e7996e15e142bee65bdc7b6
Kuzina-E <ekaterina_kuzina_7@mail.ru>
2020-12-03 18:35:28 +0300

    добавлены проверки для email

--- addressbook-web-tests/model/ContactData.cs
+++ addressbook-web-tests/model/ContactData.cs
@@ -10,10 +10,11 @@ namespace WebAddressbookTests
     public class ContactData : IEquatable<ContactData>, IComparable<ContactData>
     {
         private string allPhones;
+        private string allEmails;
 
         public ContactData()
         {
-            
+
         }
 
         public ContactData(string firstname, string lastname)
@@ -37,6 +38,12 @@ namespace WebAddressbookTests
         public string MobilePhone { get; set; }
         [Column(Name = "work")]
         public string WorkPhone { get; set; }
+        [Column(Name = "email")]
+        public string Email_1 { get; set; }
+        [Column(Name = "email2")]
+        public string Email_2 { get; set; }
+        [Column(Name = "email3")]
+        public string Email_3 { get; set; }
         [Column(Name = "deprecated")]
         public string Deprecated { get; set; }
 
@@ -61,71 +68,102 @@ namespace WebAddressbookTests
             }
         }
 
-        private string CleanUp(string phone)
+        public string AllEmails
         {
-           if (phone == null || phone == "")
+            get
             {
-                return "";
+                if (allEmails != null)
+                {
+                    return allEmails;
+                }
+                else
+                {
+                    return (ClenupEmail(Email_1) + ClenupEmail(Email_2) + ClenupEmail(Email_3)).Trim();
+                }
             }
-
-            return Regex.Replace(phone, "[-() ]", "") +"\n";
-               // phone.Replace(" ","").Replace("-","").Replace("(","").Replace(")","") + "\r\n";
-        }
-
-        public bool Equals(ContactData other)
-        {
-            if (Object.ReferenceEquals(other, null))
+            set
             {
-                return false;
-            }
-            if (Object.ReferenceEquals(this, other))
-            {
-                return true;
-
-            }
-            return FirstName == other.FirstName && LastName == other.LastName;
-        }
-
-        public override int GetHashCode()
-        {
-            return (FirstName+ LastName).GetHashCode(); 
-
-        }
-            public override string ToString()
-        {
-            return "firstname=" + FirstName + "\nlastname=" + LastName; 
-        }
- 
-        public int CompareTo(ContactData other)
-        {
-            if (Object.ReferenceEquals(other, null))
-            {
-                return 1;
-            }
-            if (LastName.CompareTo(other.LastName) != 0)
-            {
-                return LastName.CompareTo(other.LastName);
-            }
-            return FirstName.CompareTo(other.FirstName);
-        }
-
-        public List<ContactData> GetAll()
-        {
-            using (AddressbookDB db = new AddressbookDB())
-            {
-                return (from c in db.Contacts.Where(x => x.Deprecated == "0000-00-00 00:00:00") select c).ToList();
+                allEmails = value;
             }
         }
+    
 
-        public List<GroupData> GetGroups()
+
+    private string CleanUp(string phone)
+    {
+        if (phone == null || phone == "")
         {
-            using (AddressbookDB db = new AddressbookDB())
-            {
-                return (from g in db.Groups
-                        from gcr in db.GCR.Where(p => p.ContactId == Id && p.GroupId == g.Id)
-                        select g).Distinct().ToList();
-            }
+            return "";
         }
+
+        return Regex.Replace(phone, "[-() ]", "") + "\n";
+        // phone.Replace(" ","").Replace("-","").Replace("(","").Replace(")","") + "\r\n";
+    }
+
+    private string ClenupEmail(string email)
+    {
+        if (email == null || email == "")
+        {
+            return "";
+        }
+        return Regex.Replace(email, "[ -()]", "") + "\n";
+    }
+
+    public bool Equals(ContactData other)
+    {
+        if (Object.ReferenceEquals(other, null))
+        {
+            return false;
+        }
+        if (Object.ReferenceEquals(this, other))
+        {
+            return true;
+
+        }
+        return FirstName == other.FirstName && LastName == other.LastName;
+    }
+
+    public override int GetHashCode()
+    {
+        return (FirstName + LastName).GetHashCode();
 
     }
+    public override string ToString()
+    {
+        return "firstname=" + FirstName + "\nlastname=" + LastName;
+    }
+
+    public int CompareTo(ContactData other)
+    {
+        if (Object.ReferenceEquals(other, null))
+        {
+            return 1;
+        }
+        if (LastName.CompareTo(other.LastName) != 0)
+        {
+            return LastName.CompareTo(other.LastName);
+        }
+        return FirstName.CompareTo(other.FirstName);
+    }
+
+    public List<ContactData> GetAll()
+    {
+        using (AddressbookDB db = new AddressbookDB())
+        {
+            return (from c in db.Contacts.Where(x => x.Deprecated == "0000-00-00 00:00:00") select c).ToList();
+        }
+    }
+
+    public List<GroupData> GetGroups()
+    {
+        using (AddressbookDB db = new AddressbookDB())
+        {
+            return (from g in db.Groups
+                    from gcr in db.GCR.Where(p => p.ContactId == Id && p.GroupId == g.Id)
+                    select g).Distinct().ToList();
+        }
+    }
+}
+
+    
 }
